a4078f63974cc6f0d3ae584c5d269ab81a31fb5c
Gabby Vicas <gabbyvicas@Gabbys-MacBook-Pro.local>
2020-09-17 22:27:28 -0500

    added email and password to test

--- form/cypress/integration/examples/form_test.js
+++ form/cypress/integration/examples/form_test.js
@@ -5,6 +5,8 @@ describe('New user app', () => {
     })
 
     const nameInput = () => cy.get('input[name = "name"]')
+    const emailInput = () => cy.get('input[name = "email"]')
+    const passwordInput = () => cy.get ('input[name="password"]')
 
    it('checking to see if it works', ()=> {
        expect(1+2).to.equal(3)
@@ -15,6 +17,18 @@ describe('New user app', () => {
         .should('have.value', "")
         .type("Gabby")
         .should('have.value', "Gabby")
+   })
+   it('typing in email', ()=>{
+    emailInput()
+        .should ('have.value', '')
+        .type("gabbyvicas@gmail.com")
+        .should('have.value', "gabbyvicas@gmail.com")
+   })
+   it('adding password', () =>{
+       passwordInput()
+       .should('have.value', "")
+       .type("password")
+       .should('have.value', "password")
    })
 })
 
